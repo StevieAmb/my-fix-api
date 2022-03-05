bfdf1715f9d6cfcb0b3ad2a955208c828f6eb19d
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-05 11:48:05 -0700

    Update server file with get functionality

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+
+const { homeRepairs } = require('./data');
+const { application } = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.set('port', '8080');
+
+app.locals.title = 'My Fix API'
+app.locals.homeRepairs = homeRepairs;
+
+app.get('api/v1/homeRepairs', (request, response) => {
+  return response.json(app.locals.homeRepairs)
+});
