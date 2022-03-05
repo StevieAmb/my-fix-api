184b8911c8005298edf4264f245cf57e6eafe4cc
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-05 14:50:39 -0700

    Update server to send back response

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = express();
 
 app.use(express.json())
 
+app.set('port', process.env.PORT || 3001);
 app.locals.title = 'My Fix API'
+
 app.locals.homeRepairs = homeRepairs;
 
-app.set('port', process.env.PORT || 3001);
+app.get('/', (request, response) => {
+  response.send(app.locals.homeRepairs)
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is now running on ${app.get('port')}!`)
 })
-
-
-app.get('/api/v1/homeRepairs', (request, response) => {
-  response.status(200).json(app.locals.homeRepairs)
-});
