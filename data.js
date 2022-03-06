eee8acd881762b38ce75a4568d6f291d07e7607d
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-05 19:20:17 -0700

    Update data file

--- data.js
+++ data.js
@@ -92,7 +92,7 @@ const homeRepairs =  [
  },
  {
   id: 14,
-  areaOfHome: 'Kitchen',
+  areaOfHome: 'Miscellaneous',
   project: 'Fix Dishwasher',
   toolsNeeded: ['gloves', 'screwdriver', ' 6mm socket', 'pliers', 'vaccuum hose', 'reversable screwdriver', 'pick'],
   videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/lc7rh3ADZUk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
