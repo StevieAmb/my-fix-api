3908bdb15f24095dfc5edc730a0f14af0238f075
Stevie Ambroise <s.ambroise@protonmail.com>
2022-03-05 19:50:58 -0700

    Change data file

--- data.js
+++ data.js
@@ -46,126 +46,126 @@ const homeRepairs =  [
   areaOfHome: 'Bedroom',
   project: 'Remove Window Sill',
   toolsNeeded: ['6-in-1 tool', 'hammer', 'x-acto knife', 'pry bar'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/-CfUizgEGCI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/-CfUizgEGCI" 
  },
  {
   id: 8,
   areaOfHome: 'Bedroom',
   project: 'Replace Window Sill',
   toolsNeeded: ['liquid nails', 'window sill', 'gloves', 'microfiber', 'hammer'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/4YzhWtBmkfE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/4YzhWtBmkfE"
  },
  {
   id: 9,
   areaOfHome: 'Bedroom',
   project: 'Rescreen Window',
   toolsNeeded: ['flathead screwdriver', 'scissors', 'utility knife', 'spline roller', 'screen', 'spline'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UMDuYmHTabU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/UMDuYmHTabU" 
  },
  {
   id: 10,
   areaOfHome: 'Miscellaneous',
   project: 'Fix Or Replace a Doorknob',
   toolsNeeded: ['screwdriver', 'drill', 'door bolt/latch', 'doorknob', 'screws', 'flathead screwdriver', 'latch collar'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Dt2waefcQOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/Dt2waefcQOc" 
  },
  {
   id: 11,
   areaOfHome: 'Miscellaneous',
   project: 'Scratches on HardWood Floors',
   toolsNeeded: ['stain pen', 'can of stain', 'rag', 'melted wax', 'crayon', 'heat gun', 'blow dryer', 'stainable wood filler', 'painter/s tape', 'wipe-on poly', 'painter/s tool', 'sanding sponge', 'sandpaper'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/CTTTm4zT9uM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/CTTTm4zT9uM" 
  },
  {
   id: 12,
   areaOfHome: 'Miscellaneous',
   project: 'Fix Your Top-Loading Washing Machine Agitator',
   toolsNeeded: ['agitator dogs', 'needle nose pliers', '7/16th socket', '10in extension bar'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/JBhZhkQwCTA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/JBhZhkQwCTA" 
  },
  {
   id: 13,
   areaOfHome: 'Kitchen',
   project: 'Fix Kitchen Sink Strainer',
   toolsNeeded: ['plumber/s putty', 'channellock pliers', 'bucket', 'hacksaw', 'sink stopper', 'flat screwdriver', 'kitchen strainer', 'brass nut', 'paper ring', 'rubber gasket', 'top hat gasket'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/iQaDgfc-MYI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/iQaDgfc-MYI" 
  },
  {
   id: 14,
   areaOfHome: 'Miscellaneous',
   project: 'Fix Dishwasher',
   toolsNeeded: ['gloves', 'screwdriver', ' 6mm socket', 'pliers', 'vaccuum hose', 'reversable screwdriver', 'pick'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/lc7rh3ADZUk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/lc7rh3ADZUk" 
  },
  {
   id: 15,
   areaOfHome: 'Kitchen',
   project: 'Fix Cabinet Shelves',
   toolsNeeded: ['drawer corner brackets', 'screwdriver', 'screws', 'drill'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/64YlPj8ugro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/64YlPj8ugro" 
  },
 {
   id: 16,
   areaOfHome: 'Bathroom',
   project: 'Broken Toilet Handle',
   toolsNeeded: ['toilet tank lever', 'pliers'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/y_gysAOzMaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/y_gysAOzMaI" 
  },
  {
   id: 17,
   areaOfHome: 'Bathroom',
   property: 'Fix Bathroom Towel Bar',
   toolsNeeded: ['flathead screwdriver', 'piece of paper', 'phillips head screwdriver', 'drill bit'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UYYDjvPTDfs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/UYYDjvPTDfs" 
  },
  {
   id: 18,
   areaOfHome: 'Bathroom',
   project: 'Loose Toilet Seat',
   toolsNeeded: ['slotted screwdriver', 'adjustable wrench', 'measuring tape', 'replacement toilet seat', 'bolt', 'washers', 'clever nut'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/y4ql6AANARU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/y4ql6AANARU" 
  },
  {
   id: 19,
   areaOfHome: 'Bedroom',
   project: 'Fix Broken Drawers',
   toolsNeeded: ['drill', 'speed square', 'pencil', 'screws', 'drill bit', 'wood glue', 'rag'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/98gPMdsInZo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/98gPMdsInZo" 
  },
  {
   id: 20,
   areaOfHome: 'Bedroom',
   project: 'Busted Bedroom Door Latch',
   toolsNeeded: ['flathead screwdriver', 'hammer', 'phillips head screwdriver', 'metal cobalt file', 'pencil', 'toothpick', 'superglue', 'utility knife', 'hammer', 'trim nail', 'drill', 'titanium drill bit', '1in chisel', 'wood filler', 'caulk'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UcWRcbK4Ra4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/UcWRcbK4Ra4" 
  },
  {
   id: 21,
   areaOfHome: 'Bedroom',
   project: 'Sticking Bedroom Door',
   toolsNeeded: ['bucket', 'diagonal cutters', 'hammer', '8in crescent wrench', 'scraps of plywood'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/AzRqVgkjZaY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/AzRqVgkjZaY" 
  },
  {
   id: 22,
   areaOfHome: 'Miscellaneous',
   project: 'Baseboard Repairs',
   toolsNeeded: ['sandpaper', 'wood glue', 'wood filler', 'x-acto knife', 'damp rag'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VKy9YU5dBYc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/VKy9YU5dBYc" 
  },
  {
   id: 23,
   areaOfHome: 'Miscellaneous',
   project: 'Wobbly Chair Legs',
   toolsNeeded: ['drill', 'tape', 'marker', 'chisel', 'sandpaper', 'wood glue', 'artist paintbrush', 'rubber mallet'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/dKqIVWTpIiA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/dKqIVWTpIiA" 
  },
  {
   id: 24,
   areaOfHome: 'Miscellaneous',
   project: 'Remove Stain From Carpet',
   toolsNeeded: ['denatured alcohol', 'old toothbrush', 'light-colored rags', 'wet/dry vaccuum', 'baking soda', 'spoon', 'dish soap', 'water', 'spray bottle', 'acetone', 'paper towels', 'hydrogen peroxide'],
-  videos: '<iframe width="560" height="315" src="https://www.youtube.com/embed/ZDEscYyxZ0U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+  videos: "https://www.youtube.com/embed/ZDEscYyxZ0U" 
  }
 ]
 
