459514afae34a63ffcb1d2996c4e817b4f8d0ef5
Stevie Ambroise <s.ambroise@protonmail.com>
2023-05-29 13:49:53 -0600

    Allow multiple origins

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json())
-app.use(cors({
-  origin: 'http://localhost:3000/'
-}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:3000', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'My Fix API'
