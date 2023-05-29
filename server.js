83f25915b7c286467cecb66e00f2d28287927932
Stevie Ambroise <s.ambroise@protonmail.com>
2023-05-29 13:46:25 -0600

    Update origin for client request

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require('cors');
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000/'
+}));
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'My Fix API'
