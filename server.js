703d862d0ed51200c3151f4d0abf61bd38c07de3
Stevie Ambroise <s.ambroise@protonmail.com>
2023-05-29 14:01:19 -0600

    Fix cors issue

--- server.js
+++ server.js
@@ -6,25 +6,25 @@ const cors = require('cors');
 app.use(express.json())
 app.use(cors());
 
-app.use((req, res, next) => {
-  const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:3000', 'my-fixings.netlify.app'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:3000', 'my-fixings.netlify.app'];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//        res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   return next();
+// });
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'My Fix API'
 
 app.locals.homeRepairs = homeRepairs;
 
-app.get('/', (response) => {
-  response.set('Access-Control-Allow-Origin', '*');
+app.get('/', (req, response) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   response.send(app.locals.homeRepairs)
   console.log(app.locals.homeRepairs)
 });
