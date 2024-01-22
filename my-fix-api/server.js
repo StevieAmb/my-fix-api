852ada7ced7846da13aaecaddbe00a1e7de54547
Stevie Ambroise <s.ambroise@protonmail.com>
2024-01-22 15:38:47 -0700

    Add changes to file

--- my-fix-api/server.js
+++ my-fix-api/server.js
@@ -3,25 +3,22 @@ const { homeRepairs } = require('./data');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
-
 app.use(express.json())
+app.use(cors({
+  origin: '*'
+}));
 
-// app.use(cors({
-//   origin: '*'
-// }));
-
-// app.use((req, res, next) => {
-//   const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:3000', 'my-fixings.netlify.app'];
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//        res.setHeader('Access-Control-Allow-Origin', origin);
-//   }
-//   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   return next();
-// });
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:3000', 'my-fixings.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'My Fix API'
