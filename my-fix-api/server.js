68163d763fed9393ffecba2e14365bfcb80d3b71
Stevie Ambroise <s.ambroise@protonmail.com>
2024-01-22 16:36:11 -0700

    Allow all origins

--- my-fix-api/server.js
+++ my-fix-api/server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:3000', 'my-fixings.netlify.app'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
+       res.setHeader('Access-Control-Allow-Origin', '*');
   }
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
